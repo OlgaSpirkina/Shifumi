aae11c474e016ac9bc450ccd669fabc4a10bb808
olga <>
2021-06-09 22:37:58 +0200

    change gif animation

--- js/random.js
+++ js/random.js
@@ -4,17 +4,17 @@ let textOfRandomChoice = document.getElementById('text_of_random_choice');
 let pcChoice = textOfRandomChoice.value;
 function firstChooseScissors(){
 let iChooseScissors = document.getElementById('image');
-iChooseScissors.innerHTML = '<i class="far fa-hand-scissors"></i>';
+iChooseScissors.innerHTML = '<i class="far fa-5x fa-hand-scissors"></i>';
 myChoice = "ciseaux";
 }
 function firstChooseRock(){
 let iChooseRock = document.getElementById('image');
-iChooseRock.innerHTML = '<i  class="far fa-hand-rock" ></i>';
+iChooseRock.innerHTML = '<i  class="far fa-5x fa-hand-rock" ></i>';
 myChoice = "pierre";
 }
 function firstChoosePaper(){
 let iChoosePaper = document.getElementById('image');
-iChoosePaper.innerHTML = '<i class="far fa-hand-paper"></i>';
+iChoosePaper.innerHTML = '<i class="far fa-5x fa-hand-paper"></i>';
 myChoice = "papier";
 }
 
@@ -24,15 +24,15 @@ function getRndInteger() {
   let randomChoice = document.getElementById('randomChoice');
   document.getElementById('demo').innerHTML = randomNumber;
   if(randomNumber == 0){
-    randomChoice.innerHTML = '<i class="far fa-hand-rock"></i>';
+    randomChoice.innerHTML = '<i class="far fa-5x fa-hand-rock"></i>';
     console.log(randomNumber);
     pcChoice = "pierre";
   }else if(randomNumber == 1){
-    randomChoice.innerHTML = '<i class="far fa-hand-paper"></i>';
+    randomChoice.innerHTML = '<i class="far fa-5x fa-hand-paper"></i>';
     pcChoice = "papier";
     console.log(randomNumber);
   }else{
-    randomChoice.innerHTML =  '<i class="far fa-hand-scissors"></i>';
+    randomChoice.innerHTML =  '<i class="far fa-5x fa-hand-scissors"></i>';
     pcChoice = "ciseaux";
     console.log(randomNumber);
   }
